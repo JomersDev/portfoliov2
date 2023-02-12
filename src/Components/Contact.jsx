8529edc6fc74086c825f442dda1c1ca93ce76058
James McGrory <james.mcgrory135@gmail.com>
2023-02-12 18:57:11 +1100

    contact form desktop responsive

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,58 +2,65 @@ import React from "react";
 
 export default function() {
     return (
-        <section className="pb-10 px-10">
+        <section className="pb-10 px-10 sm:px-16">
             <h2 className="text-3xl mb-10 text-teal-400 font-medium underline underline-offset-8">
                 Contact Me
             </h2>
-            <div className="bg-gray-100 py-8 px-6 shadow-lg rounded-lg">
-                <form className="text-black space-y-6">
-                    <div>
-                        <label htmlFor="name" className="block text-lg font-medium text-gray-900">
-                            Your Name
-                        </label>
-                        <div className="mt-1">
-                            <input 
-                                id="name" 
-                                name="name" 
-                                type="text" 
-                                placeholder="John Smith"
-                                className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm  focus:outline-none focus:border-teal-500 "
-                            />
+            <div className="bg-gray-100 py-8 px-6 shadow-lg rounded-lg w-full md:flex md:gap-x-12">
+                <div className="w-full">
+                    <form className="text-black space-y-6">
+                        <div>
+                            <label htmlFor="name" className="block text-lg font-medium text-gray-900">
+                                Your Name
+                            </label>
+                            <div className="mt-1">
+                                <input 
+                                    id="name" 
+                                    name="name" 
+                                    type="text" 
+                                    placeholder="John Smith"
+                                    className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm  focus:outline-none focus:border-teal-500 "
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <label htmlFor="email" className="block text-lg font-medium text-gray-900  ">
-                            Email Address
-                        </label>
-                        <div className="mt-1">
-                            <input 
-                                id="email" 
-                                name="email" 
-                                type="email" 
-                                placeholder="john.smith@email.com"
-                                className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-teal-500"
-                            />
+                        <div>
+                            <label htmlFor="email" className="block text-lg font-medium text-gray-900  ">
+                                Email Address
+                            </label>
+                            <div className="mt-1">
+                                <input 
+                                    id="email" 
+                                    name="email" 
+                                    type="email" 
+                                    placeholder="john.smith@email.com"
+                                    className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-teal-500"
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <label htmlFor="message" className="block text-lg font-medium text-gray-900 ">
-                            Message
-                        </label>
-                        <div className="mt-1">
-                            <textarea 
-                                id="message" 
-                                name="message"
-                                placeholder="Hi, Write me a message here" 
-                                className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-teal-500"
-                            />
+                        <div>
+                            <label htmlFor="message" className="block text-lg font-medium text-gray-900 ">
+                                Message
+                            </label>
+                            <div className="mt-1">
+                                <textarea 
+                                    id="message" 
+                                    name="message"
+                                    placeholder="Hi, Write me a message here"
+                                    rows="6" 
+                                    className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-teal-500"
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <button type="submit" className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white rounded-md py-2 w-full">
-                        Send Message
-                    </button>
-
-                </form>
+                        <button type="submit" className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white rounded-md py-2 w-full">
+                            Send Message
+                        </button>
+                    </form>
+                </div>
+                <div className="text-black text-center w-full">
+                    <p className="hidden md:inline-flex pt-6">
+                        Feel free to send me a message, i'd love to hear from you. Simply fill out the form to the left or reach me through one of my socials below.
+                    </p>
+                </div>
             </div>
         </section>
     )
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,7 +3,7 @@ import ProjectCard from "../Components/ProjectCard";
 
 export default function() {
     return (
-        <section className="pb-10 px-10 sm:px-16">
+        <section className="pb-10 px-10">
             <h2 className="text-3xl mb-10 text-teal-400 font-medium underline underline-offset-8">My Projects</h2>
             <p>Here are some of the projects I have completed recently</p>
             <div className="my-6 flex items-center flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
