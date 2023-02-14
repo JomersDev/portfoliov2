38c4391b805a25bb125629daac3a7d2ba92705cb
James McGrory <james.mcgrory135@gmail.com>
2023-02-14 20:49:20 +1100

    added mobile nav closing functionality on screen resize

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { BsFillMoonStarsFill} from 'react-icons/bs'
 import { AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
@@ -6,29 +6,45 @@ export default function Test() {
 
     const [nav, setNav] = useState(false)
 
+    {/* opens and closes the mobile nav menu*/}
     function handleNav() {
         console.log('test click')
         setNav(!nav)
     }
+
+    {/* This closes the mobile nav if it is still open once the screen width is larger than 768px */}
+    useEffect(() => {
+        const x = window.matchMedia("(max-width: 768px)")
+        function myFunction(e) {
+          setNav(false);
+        };
+        x.addListener(myFunction)
+        return () => x.removeListener(myFunction);
+      }, []);
+
+
     return (
-        <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+        <div className="flex justify-between items-center h-24 mx-auto px-4 text-white">
             <h1 className="w-full text-3xl font-bold text-white">James McGrory</h1>
+
+            {/* desktop nav menu */}
             <ul className="hidden md:flex">
-                <li>Home</li>
-                <li>Company</li>
-                <li>Resources</li>
-                <li>About</li>
-                <li>Contact</li>
+                <li className="p-4 hover:text-teal-200 hover:underline cursor-pointer ease-in-out duration-500">About</li>
+                <li className="p-4 hover:text-teal-200 hover:underline cursor-pointer ease-in-out duration-500">Projects</li>
+                <li className="p-4 hover:text-teal-200 hover:underline cursor-pointer ease-in-out duration-500">Contact</li>
             </ul>
+
+            {/* render open or close menu based on the state of nav  */}
             <div onClick={handleNav} className="block md:hidden">
                 {nav ? <AiOutlineClose size={20} />: <AiOutlineMenu size={20} />}
             </div>
+
+            {/* Mobile nav menu */}
             <ul className={nav ? 'fixed left-0 top-0 w-[60%] bg-gray-900 h-full border-r border-r-gray-800 ease-in-out duration-500' : 'ease-in-out duration-1000 fixed left-[-100%] top-0 h-full w-[60%]'}>
                 <h1 className="w-full text-3xl font-bold text-white m-4">James McGrory</h1>
-                <li className="p-4 border-b border-gray-600">Home</li>
-                <li className="p-4 border-b border-gray-600">Company</li>
-                <li className="p-4 border-b border-gray-600">Resources</li>
                 <li className="p-4 border-b border-gray-600">About</li>
+                <li className="p-4 border-b border-gray-600">Projects</li>
+                <li className="p-4 border-b border-gray-600">Contact</li>
             </ul>
         </div>
     )
