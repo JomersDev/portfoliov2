d809ef1a7a904f229f4b005e668ffa9893e7035d
James McGrory <james.mcgrory135@gmail.com>
2023-03-03 18:33:09 +1100

    updated image file path for project card thumbnail

--- src/data.js
+++ src/data.js
@@ -3,19 +3,19 @@ const projects = [
         id: "1",
         name: "Coin Stats",
         summary: "An app that uses coin gecko API to fetch current market data",
-        image: "../src/Images/coin-stats-1.png"
+        image: "/coin-stats-1.png"
     },
     {
         id: "2",
         name: "Quiz App",
         summary: "An app that uses the Open Trivia API to generate a short quiz game",
-        image: "../src/Images/coin-stats-1.png"
+        image: "/coin-stats-1.png"
     },
     {
         id: "3",
         name: "Coffee Shop",
         summary: "A functioning E-commerce store for a fictional coffee brand",
-        image: "../src/Images/coin-stats-1.png"
+        image: "/coin-stats-1.png"
     }
 ]
 
