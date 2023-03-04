2074b994ed41c33b9e97c199e498cf079f998c09
James McGrory <james.mcgrory135@gmail.com>
2023-03-04 16:09:28 +1100

    The Brew Box page now fully responsive

--- src/Pages/TheBrewBox.jsx
+++ src/Pages/TheBrewBox.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 export default function ProjectInfo() {
 
     return (
-        <div className="px-10 flex flex-col my-6">
-            <div>
+        <div className="flex flex-col my-6 max-w-3xl mx-auto px-10">
+            <div className="max-w-4xl">
                 <img src="/the-brew-box/the-brew-box.png" alt="the brew box home page" className="rounded-md"/>
             </div>
             <div>
-                <h2 className="text-3xl mb-4 text-teal-400 font-medium underline underline-offset-8 mt-4">
+                <h2 className="text-3xl mb-4 text-teal-400 font-medium underline underline-offset-8 mt-4 md:text-4xl">
                     The Brew Box
                 </h2>
                 <ul className="flex space-x-2">
@@ -27,22 +27,22 @@ export default function ProjectInfo() {
                 </div>
             </div>
             <div>
-                <h2 className="text-xl mb-4 text-teal-400 underline underline-offset-8 mt-6">
+                <h2 className="text-xl mb-4 text-teal-400 underline underline-offset-8 mt-6 md:text-2xl">
                     Overview
                 </h2>
-                <p className="text-gray-100 text-sm leading-5 mb-4">
+                <p className="text-gray-100 text-sm leading-5 mb-4 md:text-base">
                     This ecommerce website was developed with React and Tailwind CSS for a fictional small coffee bean business. Users 
                     are able to view all products, view detailed product information and add selected items to a cart.
                 </p>
-                <div>
+                <div className="max-w-4xl">
                     <img src="/the-brew-box/all-coffee.png" alt="all coffee page" className="rounded-md"/>
                 </div>
             </div>
             <div>
-                <h2 className="text-xl mb-4 text-teal-400 underline underline-offset-8 mt-6">
+                <h2 className="text-xl mb-4 text-teal-400 underline underline-offset-8 mt-6 md:text-2xl">
                     Features
                 </h2>
-                <ul className="list-disc ml-6 text-gray-100 text-sm">
+                <ul className="list-disc ml-6 text-gray-100 text-sm md:text-base">
                     <li>Products are sorted into different categories that customers can browse</li>
                     <li className="pt-2">Customers can view detailed product information</li>
                     <li className="pt-2">From the cart, customers can increase or decrease the quantity of each product</li>
@@ -51,32 +51,32 @@ export default function ProjectInfo() {
                 </ul>
             </div>
             <div>
-                <h2 className="text-xl mb-4 text-teal-400 underline underline-offset-8 mt-6">
+                <h2 className="text-xl mb-4 text-teal-400 underline underline-offset-8 mt-6 md:text-2xl">
                     Purpose and Goal
                 </h2>
-                <p className="text-gray-100 text-sm leading-5">
+                <p className="text-gray-100 text-sm leading-5 md:text-base">
                     As a developer, I built this fictional ecommerce website to showcase my skills in creating professional and user friendly
                     websites. I also wanted to improve and refine my skills with React, React Router, React Context and Tailwind CSS. 
                 </p>
-                <div className="my-4">
+                <div className="my-4 max-w-4xl">
                     <img src="/the-brew-box/detail.png" alt="all coffee page" className="rounded-md"/>
                 </div>
-                <p className="text-gray-100 text-sm mt-2 leading-5">
+                <p className="text-gray-100 text-sm mt-2 leading-5 md:text-base">
                     As always, when starting a new project I like to view some designs from dribbble and take inspiration from modern and
                     sleek designs. This allowed me to brainstorm some ideas for how I want my project to look and the overall structure of the website. 
                     I also viewed some current real coffee bean businesses and how they present their information to their customers.
                 </p>
             </div>
             <div>
-                <h2 className="text-xl mb-4 text-teal-400 underline underline-offset-8 mt-6">
+                <h2 className="text-xl mb-4 text-teal-400 underline underline-offset-8 mt-6 md:text-2xl">
                     Highlight
                 </h2>
-                <p className="text-gray-100 text-sm leading-5">
+                <p className="text-gray-100 text-sm leading-5 md:text-base">
                     The feature that I am most proud of for this project was the shopping cart functionality. After diving deeper into React context I was
                     able to create a shopping cart that was accessible from any page on the website. Learning how this works is a valuable skill, as it can 
                     easily be replicated in other projects down the line.
                 </p>
-                <div className="mt-4">
+                <div className="mt-4 max-w-4xl">
                     <img src="/the-brew-box/cart.png" alt="all coffee page" className="rounded-md"/>
                 </div>
             </div>
