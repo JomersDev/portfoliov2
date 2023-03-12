30b1b6a2cc8ad53a52f501608f137f3ca7ac4202
James McGrory <james.mcgrory135@gmail.com>
2023-03-12 14:15:05 +1100

    added resume button to navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from "react";
-import { BsFillMoonStarsFill} from 'react-icons/bs'
 import { AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 import { Link } from "react-scroll";
 
@@ -28,7 +27,7 @@ export default function Test() {
             <h1 className="w-full text-3xl font-bold text-white">James McGrory</h1>
 
             {/* desktop nav menu */}
-            <ul className="hidden md:flex">
+            <ul className="hidden md:flex md:items-center">
                 <li className="p-4 hover:text-teal-200 hover:underline cursor-pointer ease-in-out duration-500">
                    <Link to="about" spy={true} smooth={true} offset={50} duration={500}>About</Link> 
                 </li>
@@ -38,6 +37,11 @@ export default function Test() {
                 <li className="p-4 hover:text-teal-200 hover:underline cursor-pointer ease-in-out duration-500">
                    <Link to="contact" spy={true} smooth={true} offset={50} duration={500}>Contact</Link> 
                 </li>
+                <li className="px-4">
+                   <button className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white rounded-md py-1 px-4 font-semibold cursor-pointer hover:scale-105 transition-all duration-200">
+                       Resume
+                    </button>
+                </li>
             </ul>
 
             {/* render open or close menu based on the state of nav  */}
@@ -46,11 +50,12 @@ export default function Test() {
             </div>
 
             {/* Mobile nav menu */}
-            <ul className={nav ? 'fixed left-0 top-0 w-[60%] bg-gray-900 h-full border-r border-r-gray-800 ease-in-out duration-500' : 'ease-in-out duration-1000 fixed left-[-100%] top-0 h-full w-[60%]'}>
+            <ul className={nav ? 'fixed left-0 top-0 w-[60%] bg-gray-900 h-full border-r border-r-gray-800 ease-in-out duration-500' : 'bg-gray-900 ease-in-out duration-1000 fixed left-[-100%] top-0 h-full w-[60%]'}>
                 <h1 className="w-full text-3xl font-bold text-white m-4">James McGrory</h1>
                 <li className="p-4 border-b border-gray-600">About</li>
                 <li className="p-4 border-b border-gray-600">Projects</li>
                 <li className="p-4 border-b border-gray-600">Contact</li>
+                <li className="p-4 border-b border-gray-600">Resume</li>
             </ul>
         </div>
     )
