6fec47ac42f4078de68e1707dc6dd1226058e4f3
James <james.mcgrory135@gmail.com>
2023-04-15 11:37:33 +1000

    fixed visual glitch where the Y in McGrory was cut off at the bottom

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
     return (
         <section className="background h-[90vh] flex justify-center items-center">
             <div className="text-center">
-                <h1 className="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-cyan-300 to-teal-500 md:text-7xl">
+                <h1 className="font-extrabold text-transparent text-5xl pb-6 bg-clip-text bg-gradient-to-r from-cyan-300 to-teal-500 md:text-7xl">
                     James McGrory
                 </h1>
                 <h2 className="text-2xl py-2 text-white md:text-3xl">
